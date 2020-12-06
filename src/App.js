6dcd7f172d6a101742e301c3bf1b79be56d1bd6b
Doluwamu <dboyadeitan@gmail.com>
2020-12-06 18:37:19 +0100

    added more features

--- src/App.js
+++ src/App.js
@@ -5,28 +5,31 @@ import { secrets } from "./secrets";
 const App = () => {
   const [value, setValue] = useState("");
   const [recipes, setRecipes] = useState(null);
-  console.log(recipes[0]);
+
+  console.log(recipes);
 
   const API_ID = secrets.API_ID;
   const API_KEY = secrets.API_KEY;
 
   useEffect(() => {
     getRecipes();
-  }, []);
+  });
 
   const getRecipes = async () => {
     const response = await fetch(
       `https://api.edamam.com/search?q=chicken&app_id=${API_ID}&app_key=${API_KEY}`
     );
     const data = await response.json();
-    return setRecipes(data.hits);
     // console.log(data.hits);
+    setRecipes(data.hits);
   };
 
   const handleChange = (e) => {
     e.preventDefault();
     setValue(e.target.value);
   };
+
+  const id = Math.random().toString().replace("0.", "");
 
   return (
     <div className="app">
@@ -41,7 +44,17 @@ const App = () => {
           Search
         </button>
       </div>
-      <div className="body">{recipes && recipes.map((r) => {})}</div>
+      <div className="body">
+        {recipes &&
+          recipes.length > 0 &&
+          recipes.map((r) => {
+            return (
+              <div key={id}>
+                <img src={r.recipe.image} alt="" />
+              </div>
+            );
+          })}
+      </div>
     </div>
   );
 };
--- src/secrets.js
+++
@@ -1,4 +0,0 @@
-export const secrets = {
-  API_ID: "f2c6d900",
-  API_KEY: "52a85c50851a58e26796a8d49c73f122",
-};
